599c2e7c6d1626cbb60dffc5f670c8ea51394632
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-09-10 22:02:40 +0200

    feat: setup functions utility object, add proper replacement logic.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React, { useState } from 'react'
-import './App.css'
-import { DebounceInput } from 'react-debounce-input'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { DebounceInput } from 'react-debounce-input';
 
-const regexForTags = '/{{ (\\w+)\\/(\\w+) }}/g'
+const regexForTags = /{{ (\w+)\/(\w+) }}/g;
+
+const specialTags = {};
+
+const functions = {
+  name: async (payload) => {
+    if (specialTags.hasOwnProperty(payload)) {
+      return specialTags.payload.name;
+    }
+
+    const currencies = await fetch(
+      `https://api.coinpaprika.com/v1/search/q=${payload}`
+    );
+
+    const searchedCurrency = currencies.filter(
+      (currency) => currency.symbol === payload
+    )[0];
+
+    specialTags.payload = { name: searchedCurrency.name };
+
+    return specialTags.payload.name;
+  },
+};
+
+const replacer = (match, p1, p2, p3, offset, string) => {
+  return 'swapped';
+};
 
 function App() {
-  const [text, setText] = useState('')
-  const specialTags = {}
+  const [text, setText] = useState('');
 
   useEffect(() => {
-    text.match(regexForTags).forEach((specialTag) => {
-      if (!specialTags.hasOwnProperty(specialTag)) {
-        specialTags[specialTag] = 'fetched from api'
+    if (text) {
+      const tagsToReplace = text.match(regexForTags);
+      console.log(tagsToReplace);
+      if (tagsToReplace) {
+        for (const specialTag of tagsToReplace) {
+          // tag format is: {{ func/argument }}
+          const functionWithArgument = specialTag.split(' ')[1];
+          const apiFunctionName = functionWithArgument
+            .split('/')[0]
+            .toLowerCase();
+          const argument = functionWithArgument.split('/')[1];
+
+          setText(text.replace(regexForTags, replacer));
+        }
       }
-    })
-    text.replace(regexForTags)
-  }, text)
+    }
+  }, [text]);
 
   return (
     <div className="app">
@@ -24,7 +59,7 @@ function App() {
           <DebounceInput
             element="textarea"
             onChange={(event) => {
-              setText(event.target.value)
+              setText(event.target.value);
             }}
             debounceTimeout={300}
             className="editor__window"
@@ -32,17 +67,15 @@ function App() {
           ->
           <DebounceInput
             element="textarea"
-            onChange={() => {
-              setText(text + 1)
-              console.log(state)
-            }}
+            value={text}
             debounceTimeout={300}
             className="editor__window"
+            onChange={() => {}}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
