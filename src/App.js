95aa6101cb886b7724014091b2439aafbcc55eaa
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-09-10 20:49:55 +0200

    feat: add basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { DebounceInput } from 'react-debounce-input'
+
+const regexForTags = '/{{ (\\w+)\\/(\\w+) }}/g'
 
 function App() {
+  const [text, setText] = useState('')
+  const specialTags = {}
+
+  useEffect(() => {
+    text.match(regexForTags).forEach((specialTag) => {
+      if (!specialTags.hasOwnProperty(specialTag)) {
+        specialTags[specialTag] = 'fetched from api'
+      }
+    })
+    text.replace(regexForTags)
+  }, text)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="editor">
+          <DebounceInput
+            element="textarea"
+            onChange={(event) => {
+              setText(event.target.value)
+            }}
+            debounceTimeout={300}
+            className="editor__window"
+          />
+          ->
+          <DebounceInput
+            element="textarea"
+            onChange={() => {
+              setText(text + 1)
+              console.log(state)
+            }}
+            debounceTimeout={300}
+            className="editor__window"
+          />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
